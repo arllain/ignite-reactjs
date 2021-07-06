8f94f11fb550a549a6269184afd0694bde8696d0
arllain <arllain@gmail.com>
2021-07-06 03:42:34 -0300

    Servindo HTML estático

--- github-explorer/webpack.config.js
+++ github-explorer/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
@@ -7,6 +8,11 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public','index.html')
+    })
+  ],
   resolve: {
     extensions: ['.js', '.jsx'],
   },
